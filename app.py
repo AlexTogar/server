f07f3fbdde28c6f0d94e2f3d47001bb4d12014b7
AlexTogar <smirnov.togar@ya.ru>
2019-05-04 12:36:45 +0300

    comment app run

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ def hello():
     "humidity": humidity})
 
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+# if __name__ == '__main__':
+#     app.run(debug=True, use_reloader=True)
 
